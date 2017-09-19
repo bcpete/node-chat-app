095fb9806c2a1aab8c94e1580bc5abef04713b19
hometownzero <bradycpeters@gmail.com>
2017-09-18 20:19:17 -0500

    added text input for submitting messages to all users, still only shows up in the console.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 var socket = io();
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -9,4 +10,22 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('You got a new message!,', message);
+});
+
+socket.emit('createMessage',  {
+  from: "Frank",
+  text: "Hi"
+}, function (data) {
+  console.log('Got it.', data);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name="message"]').val()
+  }, function () {
+    
+  });
 });
