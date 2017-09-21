164827b82dcd96f24baf93b4e0d633c2421c6723
hometownzero <bradycpeters@gmail.com>
2017-09-20 19:56:48 -0500

    added a stylesheet, turned off autocomplete, turned on autofocus, now clears after you send a message, and send location button disables while it is sending then re-enables after.

--- public/js/index.js
+++ public/js/index.js
@@ -1,54 +1,62 @@
 var socket = io();
 
-socket.on('connect', function () {
-  console.log('Connected to server');
+socket.on('connect', function() {
+    console.log('Connected to server');
 });
 
-socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+socket.on('disconnect', function() {
+    console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function (message) {
-  console.log('You got a new message!,', message);
-  var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+socket.on('newMessage', function(message) {
+    console.log('You got a new message!,', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
 
-  jQuery('#messages').append(li);
+    jQuery('#messages').append(li);
 });
 
-socket.on('newLocationMessage', function (locationMessage) {
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current location</a>')
-  li.text(`${locationMessage.from}: `);
-  a.attr('href', `${locationMessage.url}`)
-  li.append(a);
+socket.on('newLocationMessage', function(locationMessage) {
+    var li = jQuery('<li></li>');
+    var a = jQuery('<a target="_blank">My current location</a>')
+    li.text(`${locationMessage.from}: `);
+    a.attr('href', `${locationMessage.url}`)
+    li.append(a);
 
-  jQuery('#messages').append(li);
+    jQuery('#messages').append(li);
 });
 
-jQuery('#message-form').on('submit', function (e) {
-  e.preventDefault();
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
 
-  socket.emit('createMessage', {
-    from: 'User',
-    text: jQuery('[name="message"]').val()
-  }, function () {
-    
-  });
+    var messageTextbox = jQuery('[name=message]');
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: messageTextbox.val()
+    }, function() {
+        messageTextbox.val('');
+    });
 });
 
 var locationButton = jQuery('#send-location');
-locationButton.on('click', function () {
-  if(!navigator.geolocation) {
-    return alert('Geolocation not supported by your browser :(');
-  }
+locationButton.on('click', function() {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not supported by your browser :(');
+    }
 
-  navigator.geolocation.getCurrentPosition(function (position) {
-    socket.emit('createLocationMessage', {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send Location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        locationButton.removeAttr('disabled').text('Send Location');
+        alert('Unable to fetch location :(');
     });
-  }, function () {
-    alert('Unable to fetch location :(');
-  });
+
+
 });
--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-const path     = require('path');
-const http     = require('http');
-const express  = require('express');
+const path = require('path');
+const http = require('http');
+const express = require('express');
 const socketIO = require('socket.io');
-const {generateMessage, generateLocationMessage} = require('./utils/message');
+const { generateMessage, generateLocationMessage } = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -14,28 +14,28 @@ var io = socketIO(server);
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
+    console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app!'));
 
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined!'));
 
-  socket.on('createMessage', (newMessage, callback) => {
-    console.log('createMessage', newMessage);
-    
-    io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
-    callback('This is from the server.');
-  });
+    socket.on('createMessage', (newMessage, callback) => {
+        console.log('createMessage', newMessage);
 
-  socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords));
-  });
+        io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+        callback();
+    });
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
-  });
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords));
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
 });
 
 server.listen(port, () => {
-  console.log(`Server is up on ${port}`);
+    console.log(`Server is up on ${port}`);
 });
