21616f1245d2a30c5e5dd77c369a07b3a1871e50
hometownzero <bradycpeters@gmail.com>
2017-09-25 16:46:26 -0500

    refactored index to use mustache.js templates, and changed the way messages display. No longer looks like hot garbage.

--- public/js/index.js
+++ public/js/index.js
@@ -9,24 +9,27 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('You got a new message!,', message);
     var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
 
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} @ ${formattedTime}: ${message.text}`);
-
-    jQuery('#messages').append(li);
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(locationMessage) {
     var formattedTime = moment(locationMessage.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location</a>')
-    li.text(`${locationMessage.from} @ ${formattedTime}: `);
-    a.attr('href', `${locationMessage.url}`)
-    li.append(a);
-
-    jQuery('#messages').append(li);
+    var template = jQuery('#locationMessage-template').html();
+    var html = Mustache.render(template, {
+        text: locationMessage.url,
+        from: locationMessage.from,
+        createdAt: formattedTime
+    });
+    
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function(e) {
