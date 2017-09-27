df98f252632b1f94450b04c78efdd25f2d220b7f
hometownzero <bradycpeters@gmail.com>
2017-09-26 19:44:26 -0500

    added more methods for my users class, and the tests to go along with them. Next up is integration.

--- server/utils/users.js
+++ server/utils/users.js
@@ -20,7 +20,32 @@ class Users {
     return user;
   };
 
+  removeUser (id) {
+    var user = this.getUser(id);
+    if(user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    };
+
+    return user;
+  };
+  
+
+  getUser (id) {
+    // var user = this.users.filter((user) => user.id === id);
+    // return {user};
+
+    return this.users.filter((user) => user.id === id)[0];
+  };
+
+  getUserList (room) {
+    var users = this.users.filter((user) => user.room === room);
+    var namesArray = users.map((user) => user.name);
+
+    return namesArray;
+  };
 
 };
+
+
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,6 +2,25 @@ const expect  = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+  var users;
+  
+  beforeEach(() => {
+    users = new Users;
+    users.users = [{
+      id: 1,
+      name: 'Brady',
+      room: 'Goons'
+    }, {
+      id: 2,
+      name: 'Monica',
+      room: 'Bradys lady'
+    },{
+      id: 3,
+      name: 'Sean',
+      room: 'Goons'
+    }]; 
+  });
+
   it('Should add new user', () => {
     var users = new Users();
     var user  = {
@@ -12,5 +31,41 @@ describe('Users', () => {
     var resUser = users.addUser(user.id, user.name, user.room);
 
     expect(users.users).toEqual([user]);
+  });
+
+  it('Should return names for Goons', () => {
+    var userList = users.getUserList('Goons');
+    expect(userList).toEqual(['Brady', 'Sean']);
+  });
+
+  it('Should return names for Bradys lady', () => {
+    var userList = users.getUserList('Bradys lady');
+    expect(userList).toEqual(['Monica']);
+  });
+
+  it('Should remove a user from users list', () => {
+    var userRemoved = users.removeUser(1);
+    expect(userRemoved.id).toBe(1);
+    expect(users.users.length).toBe(2);
+  });
+
+  it('Should not remove a user from users list', () => {
+    var userRemoved = users.removeUser(99);
+    expect(userRemoved).toNotExist();
+    expect(users.users.length).toBe(3);
+  });
+
+  it('Should find user', () => {
+    var userId = 1;
+    var user = users.getUser(userId);
+    
+    expect(user.id).toBe(userId);
+  });
+
+  it('Should not find user', () => {
+    var userId = 99;
+    var user = users.getUser(userId);
+
+    expect(user).toNotExist();
   });
 });
